dccec16de54cd9ffabae4e2a252a41fff9c587fc
rayarlagadda <65158003+rayarlagadda@users.noreply.github.com>
2020-06-25 12:44:25 +0530

    Add files via upload

---
+++ powerBI.php
@@ -0,0 +1,55 @@
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/navbar1.php'; ?>
+
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  <section id="intro" class="clearfix">
+    <div class="container">
+
+      <div class="intro-info">
+        <h2>Microsoft PowerBI</h2>
+      </div>
+
+
+    </div>
+  </section><!-- #intro -->
+
+  <main id="main">
+      <section id="about">
+            <div class="container">
+
+              <div class="row about-extra" style="padding-top: 20px;">
+                <div class="col-lg-12 wow fadeInUp pt-5 pt-lg-0">
+                  <h3>Microsoft PowerBI</h3>
+                  <p >Extend essential administration limits in your undertaking by using Microsoft Power BI.<br>
+
+Our experts can help you with the going with<br><br>
+
+Business workshops showing limits of Power BI versus other Self-Service BI gadgets in the market<br><br>
+
+Customer persona and necessities assessment to endorse if Power BI is legitimately for you<br><br>
+
+Designing and Proof of Concepts<br><br>
+
+Accessibility to on-reason and cloud data<br><br>
+
+Data Governance<br><br>
+
+End customer getting ready</p> 
+                </div>
+                
+
+              </div>
+             
+                  </div>
+              </div> 
+
+            </div>
+      </section>
+
+  </main>
+
+
+<?php include 'includes/footer.php'; ?>
---
+++ yellowfinBI.php
@@ -0,0 +1,53 @@
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/navbar1.php'; ?>
+
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  <section id="intro" class="clearfix">
+    <div class="container">
+
+      <div class="intro-info">
+        <h2>YellowfinBI</h2>
+      </div>
+
+
+    </div>
+  </section><!-- #intro -->
+
+  <main id="main">
+      <section id="about">
+            <div class="container">
+
+              <div class="row about-extra" style="padding-top: 20px;">
+                <div class="col-lg-12 wow fadeInUp pt-5 pt-lg-0">
+                  <h3>YellowfinBI</h3>
+                  <p >The leader in innovation<br><br>
+                  You’ll never be stuck with an outdated and redundant analytics app. 
+                  Even after 15 years, we continue to be recognized as the leaders in 
+                  innovation in the analytics industry. We have had so many firsts, we’ve 
+                  lost count and, best of all, there are way more exciting developments in the pipeline</p>
+                  <p>We pride ourself on our business engagement model. 
+                   No pushy sales and no ridiculous contracts.  
+                   That combined with our simple pricing and licensing makes doing 
+                   business with us a pleasure and not a pain.</p>
+                   <p>Design flexibility, integration, and APIs are 
+                   hyper-critical for our developer community.  The same 
+                   love and attention we have given to the back-end is what 
+                   makes our front-end enterprise features so robust and powerful. </p> 
+                </div>
+                
+
+              </div>
+             
+                  </div>
+              </div> 
+
+            </div>
+      </section>
+
+  </main>
+
+
+<?php include 'includes/footer.php'; ?>
