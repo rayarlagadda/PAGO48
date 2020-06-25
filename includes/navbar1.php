cef5bcead7e713152ec526962aaa4457ef64058f
rayarlagadda <65158003+rayarlagadda@users.noreply.github.com>
2020-06-25 12:34:57 +0530

    Add files via upload

---
+++ includes/navbar1.php
@@ -0,0 +1,80 @@
+<body>
+
+  <!--==========================
+  Header
+  ============================-->
+  <header id="header" class="fixed-top" style="background: #217dbd;">
+    <div class="container">
+
+      <div class="logo float-left">
+        <!-- <h1 class="text-light"><a href="#header"><span>PAGO</span></a></h1> -->
+        <a href="#intro" class="scrollto"><img src="img/logo.png" alt="" class="img-fluid lgstle"></a>
+      </div>
+
+      <nav class="main-nav float-right d-none d-lg-block">
+        <ul id="myDIV">
+          <li class="lit active"><a href="#intro">Home</a></li>
+          <li class="lit"style = "left : 0px; "><a href="index.php#about">About Us</a></li>
+          <li class="lit"style = "left : 21px; " ><a href="index.php#newservices">Services</a></li>
+    
+
+
+          <li class="nav-item dropdown" style = "left : -3px; margin-top: 1px;">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"></a>
+                <div class="dropdown-menu" >
+                    <a href="mobile-app.php" class="dropdown-item"style="color:#00458f">Mobile App Development</a>
+                    <a href="business-intelligence.php" class="dropdown-item"style="color:#00458f">Business Intelligence</a>
+                    <a href="digital-transformation.php" class="dropdown-item"style="color:#00458f">Digital Transformation</a>             
+                    <a href="open-source.php" class="dropdown-item"style="color:#00458f">Open Source Technologies</a>                          
+                    <a href="professional-services.php" class="dropdown-item"style="color:#00458f">Professional Services</a>        
+                    <a href="product.php" class="dropdown-item"style="color:#00458f">Product Development</a>             
+                </div>
+            </li>
+
+
+
+
+
+         
+                <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Partners</a>
+                <div class="dropdown-menu">
+                <a href="yellowfinBI.php" class="dropdown-item"style="color:#00458f">YellowfinBI</a>
+                    <a href="powerBI.php" class="dropdown-item"style="color:#00458f">Microsoft PowerBI</a>
+                   
+                  
+                </div>
+            </li>
+            <li class="lit"><a href="industries.php">Industries</a></li>
+          <li class="lit"><a href="career.php">Career</a></li>
+          
+          <!-- <li><a href="#team">Team</a></li> -->
+          <!-- <li class="drop-down"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Drop Down 2</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul>
+          </li> -->
+          <li class="lit"><a href="index.php#contact">Contact Us</a></li>
+
+          <li class="sclIcn" style="background: #fff !important;"><a href="https://twitter.com/PagoAnalytics" class="twitter icns" ><i class="fa fa-twitter" style="color: #007bff ;"></i></a></li>
+             <li class="sclIcn" style="background: #fff !important;"> <a href="https://www.facebook.com/Pago-Analytics-107485520954349/?ti=as" class="facebook icns"><i class="fa fa-facebook" style="color: #007bff;"></i></a></li>
+              <li class="sclIcn" style="background: #fff !important;"><a href="https://www.instagram.com/pagoanalytics/?igshid=mcacy13vlw43" class="instagram icns"><i class="fa fa-instagram" style="color: #007bff;"></i></a></li>
+              <!-- <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a> -->
+              <li class="sclIcn" style="background: #fff !important;"><a href="https://www.linkedin.com/company/pago-analytics" class="linkedin icns" ><i class="fa fa-linkedin" style="color: #007bff;"></i></a></li>
+        </ul>
+      </nav><!-- .main-nav -->
+      
+    </div>
+  </header><!-- #header -->
---
+++ industries.php
@@ -0,0 +1,115 @@
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/navbar1.php'; ?>
+
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  <section id="intro" class="clearfix">
+    <div class="container">
+
+      <div class="intro-info">
+        <h2>INDUSTRIES</h2>
+      </div>
+
+    </div>
+  </section><!-- #intro -->
+
+  <main id="main">
+      <section id="about">
+            <div class="container">
+
+              <div class="row about-extra" style="padding-top: 20px;">
+                <div class="col-lg-12 wow fadeInUp pt-5 pt-lg-0">
+                
+                  <h4>Health Care</h4>
+                  <p align="justify">The proportion of aging population who need more personalized care 
+                      and round-the-clock treatment is increasing, propelling the need for more human resources 
+                      in hospitals. Healthcare, along with technology, will give the industry and care providers 
+                      the ability to simplify processes and offer personalized care with greater efficiency. Using 
+                      technology driven solutions to replace labor-intensive tasks that are better done by machines 
+                      can be a big time-saver.</p>
+                  </div>
+
+              
+                    </div>
+                  </div>
+                  
+
+                 
+      
+            <div class="container">
+
+              
+                <div class="col-lg-13 wow fadeInUp pt-5 pt-lg-0">
+                  <h4>Telecom</h4>
+                  <p align="justify">AI allows smartphones to perform highly sophisticated functions such 
+                          as augmented reality (remember Pokémon Go), speech recognition, indoor navigation, and even learning 
+                          the daily tasks and preferences of an individual to enable digital assistants like Siri and Alexa.Customer service chatbots, 
+                          like CenturyLink’s ‘sales assistant’, speech and voice services for customers where they can explore or buy content by speaking, 
+                          traffic classification, network optimization and orchestration, and predictive network maintenance, like AT&T’s self-healing and 
+                          self-learning hardware that’s powered by artificial intelligence are among some AI applications that are used by the leading players 
+                          and are among the emerging trends in the telecommunication sector.</p>
+                  </div>
+
+              
+                    </div>
+                  </div>
+
+                  <div class="container">
+
+              
+<div class="col-lg-13 wow fadeInUp pt-5 pt-lg-0">
+  <h4>Retail</h4>
+  <p align="justify"> Market knowledge, as well as control of data and information, is key to obtaining a competitive advantage in the retail sector. 
+                  Markets are continuing to grow and become more complex; the simple process of retailing has started to deploy more advanced retail 
+                  information systems to cope with all the transactions involved.
+                  Today, retailers need to transform their IT capabilities for multiple reasons, including:
+To increase the company’s ability to respond to the evolving marketplace through enhanced speed and flexibility.
+To collect and analyze customer data while enhancing differentiation.
+                 </p>
+  </div>
+
+
+    </div>
+  </div>
+
+  <div class="container">
+
+              
+<div class="col-lg-13 wow fadeInUp pt-5 pt-lg-0">
+  <h4>Insurance</h4>
+  <p align="justify"> Technology will once again be the differentiator for insurers who embrace technology 
+              to enhance customer experience while selling a commoditised insurance product.
+   Technology can also be further used to penetrate emerging markets. For instance, 
+   with the increasing penetration of mobile users in developing countries, a telecommunications
+    company acting as an agent to sell insurance products will soon become reality. 
+    New partnerships will be formed. 
+                 </p>
+  </div>
+
+
+    </div>
+  </div>
+
+
+
+
+                  </section>
+                
+                  </div>
+                
+                </div>
+                
+  
+              </div>
+        </section>
+  
+    </main>
+  
+  
+  <?php include 'includes/footer.php'; ?>
+  
+  
+
+                  
